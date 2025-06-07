9c7e0dc9226f5394a711310b118e37c42bf1fd6f
thidun <thduongphan2@gmail.com>
2025-06-07 15:23:27 +0700

    [US-01] Implement Get Countries API test

---
+++ src/test/java/country/CountriesData.java
@@ -0,0 +1,44 @@
+package country;
+
+public class CountriesData {
+    static String ALL_COUNTRIES_DATE= """
+        [
+              {
+                  "name": "Viet Nam",
+                  "code": "VN"
+              },
+              {
+                  "name": "USA",
+                  "code": "US"
+              },
+              {
+                  "name": "Canada",
+                  "code": "CA"
+              },
+              {
+                  "name": "UK",
+                  "code": "GB"
+              },
+              {
+                  "name": "France",
+                  "code": "FR"
+              },
+              {
+                  "name": "Japan",
+                  "code": "JP"
+              },
+              {
+                  "name": "India",
+                  "code": "IN"
+              },
+              {
+                  "name": "China",
+                  "code": "CN"
+              },
+              {
+                  "name": "Brazil",
+                  "code": "BR"
+              }
+          ]
+        """;
+}
